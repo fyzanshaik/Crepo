d4299b0fe7d34ad002659935b7802065cf4417f3
fyzanshaik <fyzan.shaik@gmail.com>
2022-11-08 23:14:19 +0530

    Pritnfgot done

--- Basics/datatypes.c
+++ Basics/datatypes.c
@@ -9,12 +9,13 @@ int main()
     //To print a string use []
     char fullname[] = "Faizan";
 
-    // printf("Age = %d\n", age);
-    // printf("price = %d\n", price);
-    // printf("letter = %s\n", letter);
-    // printf("Fullname = %s\n", fullname);
+    printf("Age = %d\n", age);
+    printf("price = %f\n", price);
+    printf("letter = %c\n", letter);
+    printf("Fullname = %s\n", fullname);
 
     //Code won't run something to do with pointers and memory will look into it later/won't print gives no output 
+    
     return 0;
 
 }
---
+++ Basics/printfuses.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\"HelloWorld\"");
+    printf("\n%s : %d!", "NICE", 6969);
+    printf("\n%s is my name and age is %d I can also write is as %f as it can be a float or double", "Faizan", 18, 18.0);
+    float uwunumber = 69.69;
+
+    printf("\nuwuwuwuwuwuwuwuwuwuuwuwuwuww number is %f", uwunumber);
+
+}
