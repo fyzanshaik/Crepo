aabde9f30ec9262ba911e3baba8ea772133aa8f7
fyzanshaik <fyzan.shaik@gmail.com>
2022-11-12 19:17:20 +0530

    Basics end

--- Basics/input.c
+++ Basics/input.c
@@ -9,3 +9,7 @@ int main()
     printf("You are %d years old", age);
     return 0;
 }
+//%lf for double, %d for integers, %c for single character 
+//For strings use %s but specify variable[x] where x is the amount of space you want to allocate 
+// or use * for unlimited
+// For strings with spaces use "fgets" function to store sentences "fgets(variable name, no.of char, stdin);"
