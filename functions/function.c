373a226312165962713e9e243bedd61499fa86d1
fyzanshaik <fyzan.shaik@gmail.com>
2022-11-16 20:07:40 +0530

    functions created

--- functions/function.c
+++ functions/function.c
@@ -3,12 +3,12 @@
 //A function is basically a collection of code that performs a specific task.
 int main()
 {
-    printf("Top");
-    sayHi();   
-    printf("Bottom");
+    sayHi("Mike", 34);
+    sayHi("Faizan Shaik", 30); 
+    sayHi("Shivi", 89);
     return 0;
 }
 //void doesn't return any information or data or value 
-void sayHi(){
-    printf("\nhello user\n");
+void sayHi(char name[], int age){
+    printf("\nhello %s and you are %d years old\n\n", name, age);
 }
